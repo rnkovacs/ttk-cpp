96b3b0ec81ad6b5f0655ce09465007bdec974b15
Réka Kovács <rekanikolett@gmail.com>
2019-03-22 11:17:20 +0100

    Add comments to argument parsing

--- 6/2-command-line-args/2-parse-args.cpp
+++ 6/2-command-line-args/2-parse-args.cpp
@@ -11,11 +11,14 @@ int main(int argc, char *argv[]) {
   std::string name = argv[0];
   std::cout << "Program name: " << name << std::endl;
 
-  // Convert argument to number.
+  // Convert argument to integer.
   int number;
   try {
-    number = std::stoi(argv[1]);
+    number = std::stoi(argv[1]); // "stoi" stands for "string to int".
   } catch (std::invalid_argument) {
+    // "std::invalid_argument" is a type of exception "std::stoi" is known to
+    // throw if it cannot convert its argument to an integer. Such information
+    // about standard functions can be found on their CppReference page.
     std::cerr << "Second argument is not a number!" << std::endl;
     return 2;
   }
