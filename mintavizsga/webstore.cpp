4acc91921a2f5a9f9b9f8185b1d725e242d79003
Réka Kovács <rekanikolett@gmail.com>
2019-05-14 14:46:32 +0200

    Create webstore.cpp

---
+++ mintavizsga/webstore.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "webstore.h"
+
+int main() {
+  /* 2
+  Webstore w;
+  w.list();
+
+  w.add("Bicikli", 20000, "XFG923", Date(2019, 4, 23));
+  w.add("Lampa", 3000, "POR873", Date(2019, 3, 21), Date(2019, 5, 2));
+  w.add("Zokni", 2000, "DVF777", Date(2019, 5, 11), Date(2019, 5, 13));
+  w.list();
+  
+  w.readFrom("termekek.txt");
+  w.list();
+  */
+  /* 3
+  w.add("Bicikli", 30000, "AOF723", Date(2019, 3, 27), Date(2019, 5, 1));
+  w.add("Konyv", 3000, "GHP194", Date(2019, 4, 11), Date(2019, 5, 19));
+  w.add("Konyv", 4000, "MNB928", Date(2019, 3, 25));
+  w.add("Konyv", 4000, "QNU556", Date(2019, 4, 4));
+
+  w.sort();
+  w.list();
+  */
+  /* 4
+  w.remove("XFG923");
+  w.remove("ABC123");
+  w.remove("DVF777");
+  w.list();
+  */
+  /* 5
+  std::cout << "Income in March 2019: " << w.income(2019, 3) 
+            << "\nIncome in April 2019: " << w.income(2019, 4)
+            << "\nIncome in 2019: " << w.income(2019, 0)
+            << std::endl;
+  */
+}
